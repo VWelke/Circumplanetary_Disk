226ec708af6a1d2fa7595a4478b050ec2e687340
VWelke <zcapvwe@ucl.ac.uk>
2024-12-12 17:17:03 +0000

    Plotted the first image.out!

--- PostProcessVisualisation.py
+++ PostProcessVisualisation.py
@@ -4,6 +4,7 @@
 
 
 # Call the image function from simpleplot.py
+import numpy as np
 import sys 
 import os
 sys.path.append('C:/Users/LHEM/radmc3d-2.0-master/radmc3d-2.0-master/python/radmc3d_tools')  # Add the path to the directory containing simpleplot.py
@@ -20,3 +21,28 @@ try:
     print("Import successful.")
 except ImportError as e:
     print("Import failed:", e)
+
+# Load the data from the .out files
+# If there are multiple .out files, load them all in a list
+
+out_path = 'C:/Users/LHEM/radmc3d-2.0-master/radmc3d-2.0-master/examples/run_simple_1/image.out'  # Specify the path to the directory containing the .out files
+
+
+# Check if the file exists in the specified directory
+if os.path.isfile(out_path):
+    print("File found in the specified directory.")
+else:
+    print("File not found in the specified directory.")
+
+# Plot it
+f = np.genfromtxt(out_path, delimiter=None)
+print("Data loaded successfully.")
+
+# Check the dimensions and structure of the loaded data
+print("Data shape:", f.shape)
+print("Data type:", f.dtype)
+print("Data sample:\n", f[:5])  # Print the first 5 rows of the data
+
+image(f)  # Call the image function to plot the data
+
+
